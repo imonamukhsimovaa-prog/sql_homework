73fadd56f07612c508e3d9c20bb54c3b600c5201
imonamukhsimovaa-prog <imonamukhsimovaa@gmail.com>
2025-09-26 12:44:51 +0500

    Create task12.sql

---
+++ lesson-12/homework/task12.sql
@@ -0,0 +1,169 @@
+Create table Person (personId int, firstName varchar(255), lastName varchar(255))
+Create table Address (addressId int, personId int, city varchar(255), state varchar(255))
+Truncate table Person
+insert into Person (personId, lastName, firstName) values ('1', 'Wang', 'Allen')
+insert into Person (personId, lastName, firstName) values ('2', 'Alice', 'Bob')
+Truncate table Address
+insert into Address (addressId, personId, city, state) values ('1', '2', 'New York City', 'New York')
+insert into Address (addressId, personId, city, state) values ('2', '3', 'Leetcode', 'California')
+
+
+select * from Person 
+select * from Address 
+SELECT 
+    P.firstName, 
+    P.lastName, 
+    A.city, 
+    A.state
+FROM Person P
+LEFT JOIN Address A
+    ON P.personId = A.personId 
+
+
+
+	Create table Employee (id int, name varchar(255), salary int, managerId int)
+Truncate table Employee
+insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', NULL)
+insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', NULL)
+
+select * from Employee 
+SELECT e.name AS Employee
+FROM Employee e
+JOIN Employee m 
+  ON e.managerId = m.id
+WHERE e.salary > m.salary;
+
+
+
+
+
+
+
+CREATE TABLE IF NOT EXISTS Person (
+    id INT,
+    email VARCHAR(255)
+);
+
+TRUNCATE TABLE Person;
+
+
+INSERT INTO Person (id, email) VALUES (1, 'a@b.com');
+INSERT INTO Person (id, email) VALUES (2, 'c@d.com');
+INSERT INTO Person (id, email) VALUES (3, 'a@b.com');
+
+
+SELECT email AS Email
+FROM Person
+GROUP BY email
+HAVING COUNT(*) > 1;
+
+
+
+
+select * from Person 
+
+
+
+DELETE FROM Person
+WHERE id NOT IN 
+   (SELECT MIN(id)
+    FROM Person
+    GROUP BY email)
+
+	CREATE TABLE boys (
+    Id INT PRIMARY KEY,
+    name VARCHAR(100),
+    ParentName VARCHAR(100)
+);
+
+CREATE TABLE girls (
+    Id INT PRIMARY KEY,
+    name VARCHAR(100),
+    ParentName VARCHAR(100)
+);
+
+INSERT INTO boys (Id, name, ParentName) 
+VALUES 
+(1, 'John', 'Michael'),  
+(2, 'David', 'James'),   
+(3, 'Alex', 'Robert'),   
+(4, 'Luke', 'Michael'),  
+(5, 'Ethan', 'David'),    
+(6, 'Mason', 'George');  
+
+
+INSERT INTO girls (Id, name, ParentName) 
+VALUES 
+(1, 'Emma', 'Mike'),  
+(2, 'Olivia', 'James'),  
+(3, 'Ava', 'Robert'),    
+(4, 'Sophia', 'Mike'),  
+(5, 'Mia', 'John'),      
+(6, 'Isabella', 'Emily'),
+(7, 'Charlotte', 'George');
+
+
+SELECT DISTINCT g.ParentName
+FROM girls g
+WHERE g.ParentName NOT IN (
+SELECT b.ParentName
+FROM boys b)
+
+
+DROP TABLE IF EXISTS Cart1;
+DROP TABLE IF EXISTS Cart2;
+GO
+
+CREATE TABLE Cart1
+(
+Item  VARCHAR(100) PRIMARY KEY
+);
+GO
+
+CREATE TABLE Cart2
+(
+Item  VARCHAR(100) PRIMARY KEY
+);
+GO
+
+INSERT INTO Cart1 (Item) VALUES
+('Sugar'),('Bread'),('Juice'),('Soda'),('Flour');
+GO
+
+INSERT INTO Cart2 (Item) VALUES
+('Sugar'),('Bread'),('Butter'),('Cheese'),('Fruit');
+GO
+
+
+
+SELECT 
+c1.Item AS [Item Cart 1],
+c2.Item AS [Item Cart 2]
+FROM Cart1 c1
+FULL OUTER JOIN Cart2 c2 ON c1.Item = c2.Item
+
+
+Create table Customers (id int, name varchar(255))
+Create table Orders (id int, customerId int)
+Truncate table Customers
+insert into Customers (id, name) values ('1', 'Joe')
+insert into Customers (id, name) values ('2', 'Henry')
+insert into Customers (id, name) values ('3', 'Sam')
+insert into Customers (id, name) values ('4', 'Max')
+Truncate table Orders
+insert into Orders (id, customerId) values ('1', '3')
+insert into Orders (id, customerId) values ('2', '1')
+
+
+
+
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o
+ON c.id = o.customerId
+WHERE o.id IS NULL
+
+
+
